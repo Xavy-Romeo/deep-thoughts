1f926ba9f6744935913763100f37873cef3c1fcf
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-06-29 09:41:06 -0700

    touch-up schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,22 +3,6 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-    type Thought {
-        _id: ID
-        thoughtText: String
-        createdAt: String
-        username: String
-        reactionCount: Int
-        reactions: [Reaction]
-    }  
-    
-    type Reaction {
-        _id: ID
-        reactionBody: String
-        createdAt: String
-        username: String
-    }
-
     type User {
         _id: ID
         username: String
@@ -26,6 +10,22 @@ const typeDefs = gql`
         friendCount: Int
         thoughts: [Thought]
         friends: [User]
+    }
+
+    type Thought {
+        _id: ID
+        thoughtText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
+
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
     }
 
     type Query {
