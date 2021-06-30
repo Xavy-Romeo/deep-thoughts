74d7cea0b57e1cd11fa1f1fd63f7ae61d2c9d667
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-06-30 15:18:50 -0700

    added friendslist

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
@@ -11,14 +13,8 @@ import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+  uri: '/graphql'
 });
 
 function App() {
@@ -26,7 +22,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
-          <Header></Header>
+          <Header />
           <div className="container">
             <Switch>
               <Route exact path="/" component={Home} />
@@ -34,11 +30,11 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/profile/:username?" component={Profile} />
               <Route exact path="/thought/:id" component={SingleThought} />
-            
+
               <Route component={NoMatch} />
             </Switch>
           </div>
-          <Footer></Footer>
+          <Footer />
         </div>
       </Router>
     </ApolloProvider>
